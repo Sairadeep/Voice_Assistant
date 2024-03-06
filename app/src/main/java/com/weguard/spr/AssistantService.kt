8fa6e72e49e7e8087bca0b12f699bc35cc603d17
Sai Pradeep Vamala <sai.vamala@wenable.com>
2024-03-06 22:46:41 +0530

    Voice Assistant with speech-to-text and text-to-speech.

---
+++ app/src/main/java/com/weguard/spr/AssistantService.kt
@@ -0,0 +1,133 @@
+package com.weguard.spr
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.os.Bundle
+import android.os.IBinder
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import java.util.Locale
+
+class AssistantService : Service() {
+
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var recognizerIntent: Intent
+    private val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this@AssistantService)
+
+    override fun onCreate() {
+        super.onCreate()
+        if (SpeechRecognizer.isRecognitionAvailable(this)) {
+            Toast.makeText(this, "Speech Recognizer available", Toast.LENGTH_SHORT)
+                .show()
+            recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            recognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            // Whatever the language the device is, the speech listener will work from that language and translate them to the default language
+            recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+            speechRecognizer.setRecognitionListener(object : RecognitionListener {
+                override fun onReadyForSpeech(params: Bundle?) {
+                    Log.d("OnReadyForSpeech", "User Ready for speaking.")
+                }
+
+                override fun onBeginningOfSpeech() {
+                    Log.d("onBeginningOfSpeech", "The user has started to speak.")
+                }
+
+                override fun onRmsChanged(rmsdB: Float) {
+                    Log.d("OnRmsChanged", "Change in the level of sound")
+                }
+
+                override fun onBufferReceived(buffer: ByteArray?) {
+                    Log.d("OnBufferReceived", "More sound has been received.")
+                }
+
+                override fun onEndOfSpeech() {
+                    Log.d("onEndOfSpeech", "Called after the user stops speaking.")
+                }
+
+                override fun onError(error: Int) {
+                    Log.d("OnError", "An network or recognition error occurred.")
+                }
+
+                override fun onResults(results: Bundle?) {
+                    val speechResults =
+                        results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    if (!speechResults.isNullOrEmpty()) {
+                        Toast.makeText(this@AssistantService, speechResults[0], Toast.LENGTH_SHORT)
+                            .show()
+                        textToSpeech = TextToSpeech(applicationContext) { status ->
+                            if (status == TextToSpeech.SUCCESS) {
+                                val speakResult = textToSpeech.setLanguage(Locale.getDefault())
+                                if (speakResult == TextToSpeech.LANG_MISSING_DATA || speakResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                    Toast.makeText(
+                                        this@AssistantService,
+                                        "Language not supported",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            } else {
+                                Toast.makeText(
+                                    this@AssistantService,
+                                    "I'm not knowing what's wrong..!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            textToSpeech.speak(
+                                "Hi, I'm Chitra",
+                                TextToSpeech.QUEUE_FLUSH,
+                                null,
+                                null
+                            )
+                        }
+                    } else {
+                        Toast.makeText(this@AssistantService, "No results", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+                override fun onPartialResults(partialResults: Bundle?) {
+                    Log.d(
+                        "OnPartialResults",
+                        "Called when partial recognition results are available."
+                    )
+                }
+
+                override fun onEvent(eventType: Int, params: Bundle?) {
+                    Log.d("OnEvent", "Reserved for adding future events $eventType")
+                }
+            })
+        } else {
+            Toast.makeText(
+                this,
+                "Speech Recognizer not available",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "Service Started...!", Toast.LENGTH_SHORT).show()
+        speechRecognizer.startListening(recognizerIntent)
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer.stopListening()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/weguard/spr/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.weguard.spr
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.weguard.spr.ui.theme.SPRTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity as Activity,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                99
+            )
+        } else {
+            Toast.makeText(this@MainActivity, "Record Audio Permission granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+        setContent {
+            SPRTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HideSpeechRecognizerAnnotation()
+                }
+            }
+        }
+        val intent = Intent(this@MainActivity, AssistantService::class.java)
+        startService(intent)
+    }
+}
+
+@Composable
+fun HideSpeechRecognizerAnnotation() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Assistant service...!", fontSize = 24.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SPRTheme {
+        HideSpeechRecognizerAnnotation()
+    }
+}
